48e70509be6a9aecd08403da23052d1a84627bd0
samuel01111 <samuel.nascimento.2015@hotmail.com>
2024-07-30 11:05:20 -0300

    feature/firebase-analytics: WeatherApi.kt refactor.

--- app/src/main/java/com/example/superweather/data/remote/WeatherApi.kt
+++ app/src/main/java/com/example/superweather/data/remote/WeatherApi.kt
@@ -1,16 +1,17 @@
 package com.example.superweather.data.remote
 
+import com.leumas.superweather.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
 
-    @GET("weather?units=metric&appid=da579bd6d6616e9a333a94a2a0589f2c")
+    @GET("weather?units=metric&appid=${BuildConfig.WEATHER_API_KEY}")
     suspend fun getWeatherData(
         @Query("q") name: String
     ) : WeatherDataDTO
 
-    @GET("weather?units=metric&appid=da579bd6d6616e9a333a94a2a0589f2c")
+    @GET("weather?units=metric&appid=${BuildConfig.WEATHER_API_KEY}")
     suspend fun getWeatherData(
         @Query("lat") lat: Double,
         @Query("lon") long: Double,
